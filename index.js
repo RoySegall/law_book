86c2b3ab21e0a4a86f582196dd2aef916ff296e7
Roy Segall <roy.segall@gizra.com>
2016-04-09 23:31:38 +0300

    Promise the hell out of the connection.

--- index.js
+++ index.js
@@ -7,48 +7,46 @@ var express = require('express');
 var app = express();
 var connection = null;
 
-app.get('/set_tables', function (req, res) {
-  var text = "";
-
-  r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+function connect() {
+  return r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
     if (err) {
       throw err;
     }
 
-    connection = conn;
-    text += "Connection started";
-    text += "<br />Setting up the DB";
+    return conn;
+  });
+}
 
-  }).then(function() {
+app.get('/set_tables', function (req, res) {
+  var text = "";
+
+  connect().then(function() {
     r.dbCreate('circuit').run(connection, function (err, result) {
       if (err) {
         throw err;
       }
-    })
-      .then(function() {
-        var db = r.db('circuit');
+    }).then(function() {
+      var db = r.db('circuit');
 
-        db.tableCreate('items').run(connection, function (err, result) {
+      db.tableCreate('items').run(connection, function (err, result) {
+        if (err) {
+          throw err;
+        }
+
+        text += "<br />Creating items table";
+      }).then(function() {
+        db.tableCreate('auctions').run(connection, function (err, result) {
           if (err) {
             throw err;
           }
 
-          text += "<br />Creating items table";
-        })
-          .then(function() {
-            db.tableCreate('auctions').run(connection, function (err, result) {
-              if (err) {
-                throw err;
-              }
-
-              text += "<br />Creating auctions table";
-            })
-              .then(function() {
-                res.send(text);
-              });
-          });
-
+          text += "<br />Creating auctions table";
+        }).then(function() {
+          res.send(text);
+        });
       });
+
+    });
   });
 });
 
@@ -56,13 +54,7 @@ app.route('/items')
   .get(function(req, res) {
     res.setHeader('Content-Type', 'application/json');
 
-    r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-      if (err) {
-        throw err;
-      }
-
-      connection = conn;
-
+    connect().then(function(connection) {
       r.db('circuit').table('items').run(connection, function(err, cursor) {
         if (err) {
           throw err;
@@ -73,27 +65,26 @@ app.route('/items')
             throw err;
           }
           res.send(JSON.stringify(result, null, 2));
-
         });
       });
     });
 
     console.log('Get list of users.');
   }).post(function(req, res) {
-    r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-      if (err) {
-        throw err;
-      }
 
-      connection = conn;
-
+    connect().then(function(connection) {
       // how to get information from the request?
       r.db('circuit').table('items').insert({
         title: 'Piano',
         price: '200',
         amount: 1
-      }).run(conn, function() {
-        res.send('Passed');
+      }).run(connection, function(err, result) {
+
+        if (err) {
+          throw err;
+        }
+
+        res.send(JSON.stringify(result, null, 2));
       });
     });
 
